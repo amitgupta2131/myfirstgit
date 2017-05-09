0baddd23f3aa7c14f597fcb69f2abee7098c7649
amitgupta2131 <amitgupta2131@gmail.com>
2017-05-09 23:00:21 +0530

    Update Hello.java

--- HelloWorld/src/Hello/Hello.java
+++ HelloWorld/src/Hello/Hello.java
@@ -5,15 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World2");
-        System.out.println("New text");
-        System.out.println("changes after online changes");
-        System.out.println("Today's changes");
-        System.out.println("My advanced changes");
-        System.out.println("My offline changes");
-		System.out.println("My online changes");
-		System.out.println("changes done before");
-		System.out.println("changes on github");
-		
 
 	}
 
