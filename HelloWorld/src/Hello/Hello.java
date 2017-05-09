8078e012a596c7bab950990fb7bf9e64961282f4
amitgupta2131 <amitgupta2131@gmail.com>
2017-05-09 23:46:04 +0530

    online changes to start afresh

--- HelloWorld/src/Hello/Hello.java
+++ HelloWorld/src/Hello/Hello.java
@@ -5,11 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World2");
-		System.out.println("My first change");
-		System.out.println("My second change");
-		System.out.println("My online change");
-		System.out.println("My anther online change");
-
+		
 	}
 
 }
