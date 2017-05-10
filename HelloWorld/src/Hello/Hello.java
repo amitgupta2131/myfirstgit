559b21a688c4e63356f00c816ab87db3bda24c77
Amit Gupta <amitgupta2131@gmail.com>
2017-05-10 16:00:10 +0530

    rolled back to original

--- HelloWorld/src/Hello/Hello.java
+++ HelloWorld/src/Hello/Hello.java
@@ -5,12 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World2");
-		System.out.println("Keep all of this");
 		
-		System.out.println("my first change");
-		
-		System.out.println("My online change");
-
 		
 	}
 
